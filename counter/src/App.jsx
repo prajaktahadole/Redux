60a6135ce6bec49be5e9d5b75c1fd26b76908d97
prajaktahadole <prajaktahadole@gmail.com>
2023-08-03 18:08:07 +0530

    knk

---
+++ counter/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+ 
+  return (
+    <>
+  
+      <h1>
+        <a>-</a>
+        <h1>0</h1>
+        <a>+</a>
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ counter/src/Redux/Reducer/index.js
@@ -0,0 +1,7 @@
+import ChangeTheNumber from "./Change";
+
+import { combineReducers } from "redux";
+
+export const rootReducer = combineReducers({
+    ChangeTheNumber : ChangeTheNumber
+})
